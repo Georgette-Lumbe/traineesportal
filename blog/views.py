1127e46cc69d9474a184a60b0864cae981b4022e
Georgette-Lumbe <lumbe.georgette@gmail.com>
2021-10-26 16:52:39 +0000

    Create and add content into base and index html file

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views import generic
 
 # Create your views here.
+
+
+class HomeList(generic.ListView):
+    """
+    Create HomeList view
+    """
+    def home(request):
+        return render(request, 'index.html')
