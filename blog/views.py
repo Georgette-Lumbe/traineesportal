2a8f1e79cc73a3a4adf7a5b4a088842d5d60db19
Georgette-Lumbe <lumbe.georgette@gmail.com>
2021-10-26 23:04:11 +0000

    Add tsuccess message for note added

--- blog/views.py
+++ blog/views.py
@@ -16,6 +16,15 @@ def notesdetails(request):
     """
     Create notes view
     """
+    if request.method == "POST":  # When the save button is clicked
+        form = NotesForm(request.Post)
+        if form.is_valid():
+            notes = Notes(user=request.user, title=request.POST['title'], description=request.POST['description'])
+            notes.save()
+            # Message when the note is added
+            messages.success(request, f"Your note from {request.user.username} was successfully added!")
+        else:
+            form = NotesForm()
 
     notes = Notes.objects.filter(user=request.user)
     context = {'notes': notes, 'form': form}
