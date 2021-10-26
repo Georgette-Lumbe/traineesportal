6e0432f05182a08c3329a09edf4b654fe37a5147
Georgette-Lumbe <lumbe.georgette@gmail.com>
2021-10-26 20:36:28 +0000

    Creates models Notes and superuser

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+# Models
+
+
+class Notes(models.Model):
+    """
+    Note Database Structure
+    """
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    description = models.TextField()
