756f248e561b9e76bd8756e1e8e8c77df67dbe54
Georgette-Lumbe <lumbe.georgette@gmail.com>
2021-10-29 11:11:51 +0000

    Add table of contents into ReadMee

--- traineesportal/urls.py
+++ traineesportal/urls.py
@@ -21,7 +21,7 @@ from django.urls.conf import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('blog.urls')),
+    path('', include('blog.urls'), name='blog-urls'),
     path('accounts/', include('allauth.urls')),
     path('summernote/', include('django_summernote.urls')),
 ]
