b956cd87db1f99cb77f768ddf3044c5e3eb0d1cc
Georgette-Lumbe <lumbe.georgette@gmail.com>
2021-10-29 19:41:08 +0000

    Add comment functionality

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.views import generic
@@ -167,11 +167,44 @@ def post_one(request, post_id):
     context = {
         "post": post,
         "comments": comments,
+        "commented": False,
         "liked": liked,
         "comment_form": CommentForm()
         }
 
     return render(request, 'post_one.html', context)
+
+
+def post(self, request, slug, *args, **kwargs):
+    queryset = Post.objects.filter(status=1)
+    post = get_object_or_404(queryset, slug=slug)
+    comments = post.comments.filter(approved=True).order_by("-created_on")
+    liked = False
+    if post.likes.filter().exists():
+        liked = True
+
+    comment_form = CommentForm(data=request.POST)
+
+    if comment_form.is_valid():
+        comment_form.instance.email = request.user.email
+        comment_form.instance.name = request.user.username
+        comment = comment_form.save(commit=False)
+        comment.post = post
+        comment.save()
+    else:
+        comment_form = CommentForm()
+
+    return render(
+            request,
+            "post_detail.html",
+            {
+                "post": post,
+                "comments": comments,
+                "commented": True,
+                "comment_form": comment_form,
+                "liked": liked
+            }
+        )
 
 
 @login_required
